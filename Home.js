61f925e6b06e2b44307d8ecbb3241f8270a7258e
Hamid Ali <hamid.ali477@gmail.com>
2015-11-29 16:16:01 -0500

    Initial setup

---
+++ Home.js
@@ -0,0 +1,17 @@
+var url_base = "http://wwwp.cs.unc.edu/Courses/comp426-f15/users/hmali/Codiad/workspace/cs426/Final/client-side";
+
+$(document).ready(function(){
+	
+	$('#report').on('click',
+		function(e){
+			e.preventDefault();
+			location.href= "Report.html";
+			
+		}
+	);
+	
+	
+	
+	
+	
+})
